225d8d7ebc4b83cda790e2ae9c1472f48efe3e80
Mahesh <maheshchhetri2020@gmail.com>
2024-07-26 03:40:02 +0530

    Updated db.ts

--- src/models/db.ts
+++ src/models/db.ts
@@ -3,12 +3,12 @@ import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
     await mongoose.connect('mongodb://localhost:27017/mydatabase', {
-    //   useNewUrlParser: true,
-    //   useUnifiedTopology: true
+      //   useNewUrlParser: true,
+      //   useUnifiedTopology: true
     });
     console.log('MongoDB connected');
   } catch (err) {
-    console.error(err);
+    console.error("MongoDB not connected", err);
     process.exit(1);
   }
 };
