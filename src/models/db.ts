936c6520ac7e27d2effc97bc06d30560c62305d4
Mahesh <maheshchhetri2020@gmail.com>
2024-08-17 00:01:29 +0530

    Added build command

--- src/models/db.ts
+++ src/models/db.ts
@@ -2,7 +2,7 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    await mongoose.connect('mongodb://localhost:27017/mydatabase', {
+    await mongoose.connect('mongodb://localhost:27017/maheshdb', {
       //   useNewUrlParser: true,
       //   useUnifiedTopology: true
     });
