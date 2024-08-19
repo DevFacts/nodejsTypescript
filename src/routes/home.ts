d909084a17b4a0fc0df98b920c680a90b3d16146
Mahesh <maheshchhetri2020@gmail.com>
2024-08-20 02:22:09 +0530

    Updated

--- src/routes/home.ts
+++ src/routes/home.ts
@@ -3,7 +3,7 @@ import { Request, Response, Router } from 'express';
 const router = Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('Welcome to the Home Route!');
+  res.send('Welcome to the Home Route! updated');
 });
 
 export default router;
